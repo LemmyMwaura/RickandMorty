1eb49368690faa064787ff398fcdbfcaad737f5e
lemmy mwaura <lemminton@gmail.com>
2023-02-03 13:27:11 +0300

    feat: add needed modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RickAndMortyListComponent } from './components/rick-and-morty-list/rick-and-morty-list.component';
 import { RickAndMortyItemComponent } from './components/rick-and-morty-item/rick-and-morty-item.component';
+import { HomeComponent } from './components/home/home.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     RickAndMortyListComponent,
-    RickAndMortyItemComponent
+    RickAndMortyItemComponent,
+    HomeComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
