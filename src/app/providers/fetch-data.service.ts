7b9361695ae9b1a4f065e71f92fbaa62838b9de5
lemmy mwaura <lemminton@gmail.com>
2023-02-03 12:28:14 +0300

    feat: fetch data from service

--- src/app/providers/fetch-data.service.ts
+++ src/app/providers/fetch-data.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FetchDataService {
-  URL = ''
-  
+  URL = 'https://rickandmortyapi.com/api/character'
+  data!:any[]
   constructor(private _http:HttpClient) { }
+
+  getData(): Observable<any> {
+    return this._http.get<any>(this.URL)
+  }
 }
