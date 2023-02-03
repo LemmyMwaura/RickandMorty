bb0b7628e0a19939a6cc2472338b7231dcef4ab7
lemmy mwaura <lemminton@gmail.com>
2023-02-03 13:26:41 +0300

    feat: add model

--- src/app/components/rick-and-morty-list/rick-and-morty-list.component.ts
+++ src/app/components/rick-and-morty-list/rick-and-morty-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchDataService } from 'src/app/providers/fetch-data.service';
+import { RickAndMorty } from 'src/app/models/rickAndMorty.model';
 
 @Component({
   selector: 'app-rick-and-morty-list',
   templateUrl: './rick-and-morty-list.component.html',
-  styleUrls: ['./rick-and-morty-list.component.scss']
+  styleUrls: ['./rick-and-morty-list.component.scss'],
 })
 export class RickAndMortyListComponent implements OnInit {
+  @Input() searchString!: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _fetchDataService: FetchDataService) {
+    this._fetchDataService
+      .getData()
+      .subscribe((res) => (this.data = res.results));
   }
 
+  data!: RickAndMorty[];
+  ngOnInit(): void {}
 }
