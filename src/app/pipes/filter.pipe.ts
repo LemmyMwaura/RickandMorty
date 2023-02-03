4bdb4f93b67007a009463666041cc251e1944d85
lemmy mwaura <lemminton@gmail.com>
2023-02-03 13:25:55 +0300

    feat: filterdata pipe

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string) {
+    if (value.length == 0 || filterString == '') return value;
+
+    const repos = [];
+    for (const repo of value) {
+      if (repo['name'].toLowerCase().includes(filterString.toLowerCase())) {
+        repos.push(repo);
+      }
+    }
+    return repos;
+  }
+}
