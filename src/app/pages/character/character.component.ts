102ee76aaa28ff401d4dd4d133bcb0f82db780f4
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-26 15:26:01 +0300

    perf: set changedetection to onpush - (will be making code more reactive)

--- src/app/pages/character/character.component.ts
+++ src/app/pages/character/character.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
@@ -10,6 +10,7 @@ import { FetchDataService } from 'src/app/providers/fetch-data.service';
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CharacterComponent implements OnInit {
   character$!: Observable<RickAndMorty>;
